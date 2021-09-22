988333c2d31c2b42438ebcef32e2ad79a953db8c
Daniekeys-official <68125935+Daniekeys@users.noreply.github.com>
2021-09-22 10:41:50 -0700

    yes

--- src/App.js
+++ src/App.js
@@ -1,53 +1,10 @@
 import React from 'react'
-import Sidemenu from './components/Sidemenu';
-import {makeStyles,CssBaseline,createMuiTheme, ThemeProvider} from '@material-ui/core'
-// import { height } from '@material-ui/system';
-import Header from './components/Header'
-import Employee from './pages/employee/Employee';
-
-
-const theme = createMuiTheme({
-palette:{
-  primary:{
-    main:'#333333',
-    light:'#3c44b126',
-  },
-  secondary:{
-    main:'#f83245',
-    light:'#f8324526'
-  },
-  background:{
-    default:'#f4f5fd',
-
-  }
-
-
-}
-})
-
-const useStyles = makeStyles({
-  appMain:{
-    paddingLeft:'320px',
-    width:'100%',
-    
-    height:'100%',
-
-
-  }
-})
-
 
 const App = () => {
-  const classes = useStyles();
   return (
-    <ThemeProvider theme={theme}>
-      <Sidemenu />
-      <div className={classes.appMain}>
-      <Header />
-        <Employee />
-      </div>
-      <CssBaseline />
-    </ThemeProvider>
+    <div>
+      i am the app component
+    </div>
   )
 }
 
